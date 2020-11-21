7794141a2f7bfb6d83d2d0055ff6cfc63877605d
Nikita <nikitasa1997@gmail.com>
2020-11-21 23:36:32 +0300

    next

--- search_engine/header.hpp
+++ search_engine/header.hpp
@@ -1,16 +1,16 @@
-#ifndef HEADER_HPP
-#define HEADER_HPP
+#ifndef SEARCH_ENGINE_HEADER_HPP
+#define SEARCH_ENGINE_HEADER_HPP
 
-using schar = signed char;
-using uchar = unsigned char;
-using shrt = short;
-using ushrt = unsigned short;
-using uint = unsigned;
-using ulong = unsigned long;
-using llong = long long;
-using ullong = unsigned long long;
-using flt = float;
-using dbl = double;
-using ldbl = long double;
+typedef signed char schar;
+typedef unsigned char uchar;
+typedef short shrt;
+typedef unsigned short ushrt;
+typedef unsigned uint;
+typedef unsigned long ulong;
+typedef long long llong;
+typedef unsigned long long ullong;
+typedef float flt;
+typedef double dbl;
+typedef long double ldbl;
 
 #endif

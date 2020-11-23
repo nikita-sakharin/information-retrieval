57a6039f822d472ce7b2474ba1ced20e76810c87
Nikita <nikitasa1997@gmail.com>
2020-11-23 20:55:58 +0300

    next

---
+++ search_engine/include/search_engine/header.hpp
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ENGINE_HEADER_HPP
+#define SEARCH_ENGINE_HEADER_HPP
+
+typedef signed char schar;
+typedef unsigned char uchar;
+typedef short shrt;
+typedef unsigned short ushrt;
+typedef unsigned uint;
+typedef unsigned long ulong;
+typedef long long llong;
+typedef unsigned long long ullong;
+typedef float flt;
+typedef double dbl;
+typedef long double ldbl;
+
+#endif

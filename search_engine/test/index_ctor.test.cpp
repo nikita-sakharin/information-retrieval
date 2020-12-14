8afc33af612c248aa4437f9a376d022949c3b520
Nikita <nikitasa1997@gmail.com>
2020-12-14 14:14:49 +0300

    next

---
+++ search_engine/test/index_ctor.test.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <search_engine/header.hpp>
+
+TEST(IndexCtorTest, FIXME) {
+    std::cout << "TokenizatorTest, WcharToChar" << std::endl;
+    ASSERT_EQ(wchar_to_char('Q'), L'q');
+}

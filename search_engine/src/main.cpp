f1d37784eff5bddebe6fdaf1324fb7b298d037ea
Nikita <nikitasa1997@gmail.com>
2020-12-24 00:02:13 +0300

    next

--- search_engine/src/main.cpp
+++ search_engine/src/main.cpp
@@ -1,74 +1,38 @@
-#include <cassert>
 #include <cstddef>
 #include <cstdlib>
-#include <cstring>
 
-#include <codecvt>
 #include <exception>
 #include <iostream>
-#include <locale>
-#include <string>
 
-#include <search_engine/header.hpp>
-#include <search_engine/memmap.hpp>
-/*
-/cygdrive/c/Users/nikit/Desktop/Nikita/Learning/IR/webcrawler/texts.json
-*/
-/*
-    using namespace std;
-    const uint i = 7 * 256 * 256 + 3 * 256 + 10;
-    const uchar * const ptr = reinterpret_cast<const uchar *>(&i);
-    cout << int(ptr[0]) << ' ' << int(ptr[1]) << ' ' << int(ptr[2]) << '\n';
-    const string str = "\u0449";
-    cout << str.size() << '\n';
+#include <search_engine/indexer.hpp>
 
-    wstring_convert<codecvt_utf8<char32_t>, char32_t> cvt;
-    u32string utf32 = cvt.from_bytes(str);
-    cout << utf32.size() << '\n';
-    cout << uint(utf32[0]) << '\n';
-*/
-using namespace std;
+int main(const int argc, char *argv[]) {
+    using std::cerr, std::exception, std::exit;
 
-int main() {
-    constexpr const char *name = "en_US.UTF-8";
-    assert(setlocale(LC_ALL, name) != nullptr);
-/*
-    for (wint_t c = 0; c < 0X110000; ++c) {
-        const wint_t lower = towlower(c);
-        assert(lower != WEOF);
-        if (c != lower)
-            cout << c << " != " << lower << '\n';
+    // --help?
+    const char *text = nullptr, *index = nullptr;
+    for (int i = 1; i < argc; ++i) {
+        if (i + 1 >= argc)
+            break;
+        if (strcmp(argv[i], "--text"))
+            text = argv[i + 1];
+        else if (strcmp(argv[i], "--index"))
+            index = argv[i + 1]
+        else
+            break;
     }
-    cout << "SUCCESS" << endl;
-*/
-    memmap m("/cygdrive/c/Users/nikit/Desktop/Nikita/Learning/IR/webcrawler/texts.json");
-    const uchar * const data = reinterpret_cast<const uchar *>(m.data());
-    const size_t size = m.size();
-    size_t total = 0;
-    for (size_t i = 0; i < size; ++i) {
-        if ((i & 0X1FFFFF) == 0)
-            cout << i << '\n';
-        ptrdiff_t count = 0;
-        if (data[i] >= 240 && data[i] < 248)
-            count = 3;
-        else if (data[i] >= 224 && data[i] < 240)
-            count = 2;
-        else if (data[i] >= 192 && data[i] < 224)
-            count = 1;
-        else if (data[i] >= 128) {
-            cerr << "data[" << i << "] = " << static_cast<int>(data[i]) << '\n';
-            continue;
-        } else if (data[i] < 128)
-            continue;
-        ++i;
-        if (count == 3)
-            cerr << "count = 3 for i = " << i << '\n';
-        for (ptrdiff_t j = 0; j < count; ++j)
-            if (data[i + j] < 128)
-                cerr << "data[" << i << "] = " << static_cast<int>(data[i]) << '\n';
-        i += count - 1;
+
+    if (text == nullptr || index == nullptr) {
+        cerr << "Usage: " << argv[0] << " --text [FILE] --index [FILE]\n";
+        exit(EXIT_FAILURE);
     }
-    cout << total << '\n';
+
+    try {
+        indexer::index(argv[1], argv[2]);
+    } catch (const std::exception &except) {
+        cerr << except.what() << '\n';
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }

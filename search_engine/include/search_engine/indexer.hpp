88d6cd191b2f67eda1177865772e2e8d98803173
Nikita <nikitasa1997@gmail.com>
2020-12-25 22:33:13 +0300

    next

--- search_engine/include/search_engine/indexer.hpp
+++ search_engine/include/search_engine/indexer.hpp
@@ -1,9 +1,11 @@
 #ifndef SEARCH_ENGINE_INDEXER_HPP
 #define SEARCH_ENGINE_INDEXER_HPP
 
+#include <search_engine/index.hpp>
+
 class indexer final {
 public:
-    static void make_index(const char *, const char *);
+    static index make_index(const char *);
 };
 
 #endif

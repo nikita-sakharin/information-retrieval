ebbc66aa740cf6fad25ff7062298af5276b0b3f9
Nikita <nikitasa1997@gmail.com>
2020-12-25 22:32:07 +0300

    next

--- search_engine/src/indexer.cpp
+++ search_engine/src/indexer.cpp
@@ -1,22 +1,10 @@
-#include <fstream> // ofstream
-#include <iostream> // ios_base
-
-#include <search_engine/index.hpp>
 #include <search_engine/indexer.hpp>
 #include <search_engine/memmap.hpp>
 
-void indexer::make_index(
-    const char * const texts_file,
-    const char * const index_file
+index indexer::make_index(
+    const char * const texts_file
 ) {
-    using std::ofstream, std::ios_base;
 
     const memmap map(texts_file);
-    index text_index;
-
-    ofstream stream(
-        index_file,
-        ios_base::binary | ios_base::out | ios_base::trunc
-    );
-    stream << text_index;
+    return index();
 }

a8dfedd79a15a9d5eb2b9ace958ca3243b54b605
Nikita <nikitasa1997@gmail.com>
2020-12-27 12:46:46 +0300

    next

--- search_engine/include/search_engine/header.hpp
+++ search_engine/include/search_engine/header.hpp
@@ -1,18 +1,16 @@
 #ifndef SEARCH_ENGINE_HEADER_HPP
 #define SEARCH_ENGINE_HEADER_HPP
 
-typedef signed char schar;
-typedef unsigned char uchar;
-typedef short shrt;
-typedef unsigned short ushrt;
-typedef unsigned uint;
-typedef unsigned long ulong;
-typedef long long llong;
-typedef unsigned long long ullong;
-typedef float flt;
-typedef double dbl;
-typedef long double ldbl;
-
-wchar_t wchar_to_char(char) noexcept;
+using schar = signed char;
+using uchar = unsigned char;
+using shrt = short;
+using ushrt = unsigned short;
+using uint = unsigned;
+using ulong = unsigned long;
+using llong = long long;
+using ullong = unsigned long long;
+using flt = float;
+using dbl = double;
+using ldbl = long double;
 
 #endif

4e8825f65570d43d6bd9d0417a72b3ce768a8258
Nikita <nikitasa1997@gmail.com>
2020-12-28 19:12:11 +0300

    next

--- search_engine/include/search_engine/index.hpp
+++ search_engine/include/search_engine/index.hpp
@@ -1,18 +1,17 @@
 #ifndef SEARCH_ENGINE_INDEX_HPP
 #define SEARCH_ENGINE_INDEX_HPP
 
-#include <iostream>
-#include <map>
-#include <string_view>
-#include <vector>
+#include <cstdint> // uint32_t
 
-#include <search_engine/header.hpp>
+#include <iostream> // istream, ostream
+#include <map> // map
+#include <string_view> // string_view
+#include <vector> // vector
 
 class index final {
-    std::map<std::string_view, uint> posting;
-    std::vector<char> dictionary;
-
 public:
+    using doc_id = uint32_t;
+
     inline index() noexcept = default;
     inline index(const index &) noexcept = default;
     inline index(index &&) noexcept = default;
@@ -20,9 +19,15 @@ public:
     inline index &operator=(index &&) noexcept = default;
     inline ~index() noexcept = default;
 
+    doc_id insert_document(const std::string_view &);
+
     // void insert(uint, uint);
     friend std::istream &operator<<(std::istream &, index &);
     friend std::ostream &operator<<(std::ostream &, const index &);
+
+private:
+    std::map<std::string_view, std::vector<doc_id>> posting;
+    std::vector<char> dictionary;
 };
 
 #endif

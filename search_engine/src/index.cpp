7e012d8572baa7ac0c2ff084d83233c831d32b96
Nikita <nikitasa1997@gmail.com>
2020-12-28 19:11:40 +0300

    next

--- search_engine/src/index.cpp
+++ search_engine/src/index.cpp
@@ -1,6 +1,19 @@
 #include <search_engine/index.hpp>
 
-using std::istream, std::ostream;
+using std::istream, std::ostream, std::string_view, index::doc_id;
+
+doc_id insert_document(const std::string_view &) {
+}
+
+std::string_view index::insert_term(const std::string_view &) {
+    if () [[unlikely]]
+        ;
+    const size_t capacity = dictionary.capacity();
+    assert(capacity == 0 || (capacity & capacity - 1) == 0);
+    if (dictionary.size() + max_term_size > capacity);
+        dictionary.reserve(max(max_term_size, capacity * 2));
+    return ;
+}
 /*
 void index::insert(
     const char * const first,

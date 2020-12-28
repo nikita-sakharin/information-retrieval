a5d6f7127c9eeac27b6797e2c75596f33fc65126
Nikita <nikitasa1997@gmail.com>
2020-12-28 18:57:39 +0300

    next

--- search_engine/src/indexer.cpp
+++ search_engine/src/indexer.cpp
@@ -1,95 +1,106 @@
 #include <cstddef> // size_t
 
+#include <string> // string
+
 #include <search_engine/index.hpp>
 #include <search_engine/indexer.hpp>
 #include <search_engine/memmap.hpp>
 
-using std::size_t, std::string_view, index::doc_id;
+using std::size_t, std::string, std::string_view, index::doc_id;
 
-static const char *parse_escape(const char *, const char *, char *);
-static const char *parse_title(const char *, const char *, char *, size_t);
+static const char *parse_escape(const char *, const char *, string &);
+static const char *parse_string(const char *, const char *, string &);
 
 index make_index(const char * const texts_file) {
-    static constexpr size_t max_size = 1UL << 21; // 2 MiB
-    static char buffer[max_size];
+    static constexpr size_t reserve = 1UL << 21; // 2 MiB = 512 * 4 KiB
+    static string buf;
     static constexpr const char *what = "make_index: invalid JSON";
 
+    buf.reserve(reserve);
     const memmap map(texts_file);
-    const char *data = map.data();
-    const char * const data_end = data + map.size();
-    if (data == data_end) [[unlikely]]
+    const char *first = map.data();
+    const char * const last = first + map.size();
+    if (first == last) [[unlikely]]
         throw logic_error("make_index: file is empty");
-    else if (*data != '{') [[unlikely]] throw logic_error(what);
-    ++data;
+    else if (*first != '{') [[unlikely]] throw logic_error(what);
+    ++first;
 
     index returns;
-    while (data < data_end) {
-        data = parse_title(data, data_end, buffer, max_size);
-        if (*data != ':') [[unlikely]] throw logic_error(what);
-        const doc_id id = index.insert_document(string_view(buffer));
-        if (*data != ',') [[unlikely]] throw logic_error(what);
-        ++data;
+    while (first < last) {
+        first = parse_string(first, last, buf);
+        if (first == last || *first != ':') [[unlikely]]
+            throw logic_error(what);
+        const doc_id id = index.insert_document(buf);
+
+        first = parse_string(first, last, buf); // TODO
+        if (first == last || *first != ',') [[unlikely]]
+            throw logic_error(what);
+        ++first;
     }
 
-    if (*data != '}') [[unlikely]] throw logic_error(what);
+    if (*first != '}') [[unlikely]] throw logic_error(what);
 
     return returns;
 }
 
 static const char *parse_escape(
-    const char * const data,
-    const char * const data_end,
-    char * const buf
+    const char * const first,
+    const char * const last,
+    string &buf
 ) {
     static constexpr const char *what = "parse_escape: invalid escape sequence";
-    switch (*data) {
-        [[likely]]   case '\"': *buf = '\"'; return data + 1;
-        [[likely]]   case '\\': *buf = '\\'; return data + 1;
-        [[unlikely]] case 'b':  *buf = '\b'; return data + 1;
-        [[likely]]   case 'f':  *buf = '\f'; return data + 1;
-        [[likely]]   case 'n':  *buf = '\n'; return data + 1;
-        [[likely]]   case 'r':  *buf = '\r'; return data + 1;
-        [[likely]]   case 't':  *buf = '\t'; return data + 1;
+    assert(first < last);
+
+    switch (*first) {
+        [[likely]]   case '\"': buf.push_back('\"'); return first + 1;
+        [[likely]]   case '\\': buf.push_back('\\'); return first + 1;
+        [[unlikely]] case 'b':  buf.push_back('\b'); return first + 1;
+        [[likely]]   case 'f':  buf.push_back('\f'); return first + 1;
+        [[likely]]   case 'n':  buf.push_back('\n'); return first + 1;
+        [[likely]]   case 'r':  buf.push_back('\r'); return first + 1;
+        [[likely]]   case 't':  buf.push_back('\t'); return first + 1;
         [[unlikely]] case 'u':
-            if (data + 4 >= data_end) [[unlikely]] throw logic_error(what);
-            const int value = (data[3] - '0') * 16 + data[4] - '0';
-            if (data[1] != '0' || data[2] != '0' || (
+            if (first + 4 >= last) [[unlikely]] throw logic_error(what);
+            const int value = (first[3] - '0') * 16 + first[4] - '0';
+            if (first[1] != '0' || first[2] != '0' || (
                 (value < 0 || value >= 8) &&
                 value != 11 &&
                 (value < 14 || value >= 32)
             )) [[unlikely]] throw logic_error(what);
-            *buf = value;
-            return data + 5;
+            buf.push_back(value);
+            return first + 5;
         [[unlikely]] default: throw logic_error(what);
     }
 }
 
-static const char *parse_title(
-    const char *data,
-    const char * const data_end,
-    char *buf,
-    const size_t size
+static const char *parse_string(
+    const char *first,
+    const char * const last,
+    string &buf
 ) {
-    static constexpr const char *what = "parse_title: invalid JSON";
+    static constexpr const char *what = "parse_string: invalid string";
+    buf.resize(0);
+    assert(first < last);
 
-    if (*data != '\"') [[unlikely]] throw logic_error(what);
-    ++data;
+    if (*first != '\"') [[unlikely]] throw logic_error(what);
+    ++first;
 
     bool is_escape = false;
-    while (data < data_end && (is_escape || *data != '\"')) {
-        // TODO check size: buffer overflow
+    while (first < last && (is_escape || *first != '\"')) {
         if (is_escape) [[unlikely]] {
             is_escape = false;
-            data = parse_escape(data, data_end, buf);
-        } else if (*data == '\\') [[unlikely]] {
+            first = parse_escape(first, last, buf);
+        } else if (*first == '\\') [[unlikely]] {
             is_escape = true;
-            ++data;
-        } else [[likely]]
-            *buf++ = *data++;
+            ++first;
+        } else [[likely]] {
+            assert(*first != '\"');
+            buf.push_back(*first++);
+        }
     }
-    if (data == data_end) // TODO
+    assert(first <= last);
+    if (first == last) [[unlikely]] throw logic_error(what);
+    assert(is_escape && *first == '\"');
 
-    if () [[unlikely]] // TODO
-        throw logic_error("make_index: JSON is valid but title is empty");
-    *buf = '\0';
+    return first + 1;
 }

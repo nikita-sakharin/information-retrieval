337268ed5a11b2fabd99027cfbbd50e84636a095
Nikita <nikitasa1997@gmail.com>
2020-12-29 20:35:36 +0300

    next

--- search_engine/src/tokenizer.cpp
+++ search_engine/src/tokenizer.cpp
@@ -1,16 +1,12 @@
 #include <cctype>
 
-#include <search_engine/tokenizator.hpp>
+#include <search_engine/tokenizer.hpp>
 
-static void index_file(const char * const filename, index_ctor &index) {
-    const memmap map(filename);
-    const char * const data = map.data(), data_end = data + map.size();
-    for (const char *ptr = data; ptr < data_end; ) {
-        ;
-    }
-        // если в тексте есть тире
-        // инженер-механик co-education
-        // или точки
-        // C.A.T. U.S.A.
-        // то они обрабатываются в токенизаторе, а не нормализаторе
+void index_file() {
 }
+
+// если в тексте есть тире
+// инженер-механик co-education
+// или точки
+// C.A.T. U.S.A.
+// то они обрабатываются в токенизаторе, а не нормализаторе

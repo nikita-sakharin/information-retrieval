67d1bd8cb0c876fbbc6eda354c3cc8452685cab0
Nikita <nikitasa1997@gmail.com>
2020-12-29 20:35:50 +0300

    next

--- search_engine/include/search_engine/tokenizer.hpp
+++ search_engine/include/search_engine/tokenizer.hpp
@@ -1,16 +1,8 @@
-#ifndef SEARCH_ENGINE_TOKENIZATOR_HPP
-#define SEARCH_ENGINE_TOKENIZATOR_HPP
-
-#include <search_engine/memmap.hpp>
+#ifndef SEARCH_ENGINE_TOKENIZER_HPP
+#define SEARCH_ENGINE_TOKENIZER_HPP
 
 class tokenizator final {
 public:
-    static void split(const memmap &, index &);
-    // если в тексте есть тире
-    // инженер-механик co-education
-    // или точки
-    // C.A.T. U.S.A.
-    // то они обрабатываются в токенизаторе, а не нормализаторе
 }
 
 #endif

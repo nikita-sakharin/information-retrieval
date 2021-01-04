a7e9d600775546f078d1cbf3ea171cb6b3c2af88
Nikita <nikitasa1997@gmail.com>
2021-01-04 15:14:59 +0300

    next

--- search_engine/include/search_engine/str_parser.hpp
+++ search_engine/include/search_engine/str_parser.hpp
@@ -14,6 +14,8 @@ public:
 
     constexpr str_parser() noexcept = default;
     constexpr explicit str_parser(std::string_view) noexcept;
+    constexpr str_parser(const char *, std::size_t) noexcept;
+    constexpr str_parser(const char *, const char *) noexcept;
     constexpr str_parser(const str_parser &) noexcept = default;
     constexpr str_parser(str_parser &&) noexcept = default;
     constexpr str_parser &operator=(const str_parser &) noexcept = default;
@@ -31,15 +33,21 @@ constexpr str_parser::str_parser(
     const std::string_view view
 ) noexcept : view_(view) {
     assert(view_.data() <= view_.data() + view_.size());
+
+    if (view_.data() < 2 || view_.front() != '\"') [[unlikely]]
+        throw logic_error("str_parser::str_parser: invalid string");
+    else
+        view_ = view_.substr(1);
 }
-/*
+
+constexpr str_parser::str_parser(
+    const char * const data,
+    const std::size_t size
+) noexcept : str_parser(std::string_view(data, size)) {}
+
 constexpr str_parser::str_parser(
     const char * const first,
     const char * const last
-) noexcept : first_(), last_() {
-    if (first_ > last_) [[unlikely]]
-        throw logic_error("memmap::file::close: file is not open");
-    assert(fildes_ >= 0);
-}
-*/
+) noexcept : str_parser(std::string_view(first, last)) {}
+
 #endif

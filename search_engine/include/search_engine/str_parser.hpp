2958d5875ffd3444e23d41d07b3ae7c2f32793c5
Nikita <nikitasa1997@gmail.com>
2021-01-04 14:26:17 +0300

    next

--- search_engine/include/search_engine/str_parser.hpp
+++ search_engine/include/search_engine/str_parser.hpp
@@ -7,31 +7,27 @@
 class str_parser final {
 public:
     using iterator_category = std::input_iterator_tag;
-    using difference_type = string_view::difference_type;
-    using value_type = string_view::value_type;
-    using pointer = string_view::pointer;
-    using reference = string_view::reference;
+    using difference_type = std::string_view::difference_type;
+    using value_type = std::string_view::value_type;
+    using pointer = std::string_view::pointer;
+    using reference = std::string_view::reference;
 
-    constexpr str_parser() noexcept;
+    constexpr str_parser() noexcept = default;
+    constexpr explicit str_parser(std::string_view) noexcept;
     constexpr str_parser(const str_parser &) noexcept = default;
     constexpr str_parser(str_parser &&) noexcept = default;
     constexpr str_parser &operator=(const str_parser &) noexcept = default;
     constexpr str_parser &operator=(str_parser &&) noexcept = default;
     constexpr ~str_parser() noexcept = default;
 
+    friend constexpr bool operator==(str_parser, str_parser) noexcept;
+    friend constexpr bool operator!=(str_parser, str_parser) noexcept = default;
+
 private:
-    string_view view_;
+    std::string_view view_;
 };
 
-constexpr str_parser::str_parser() noexcept : first_(), last_() {}
-
-constexpr str_parser::str_parser(
-    const char * const first,
-    const char * const last
-) noexcept : first_(), last_() {
-    if (first_ > last_) [[unlikely]]
-        throw logic_error("memmap::file::close: file is not open");
-    assert(fildes_ >= 0);
+constexpr explicit str_parser::str_parser(std::string_view view) noexcept : view_(view) {
 }
 
 #endif

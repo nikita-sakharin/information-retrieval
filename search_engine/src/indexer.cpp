5a81d26897d9f37878a038785f80cefe85d62337
Nikita <nikitasa1997@gmail.com>
2021-01-09 14:35:22 +0300

    next

--- search_engine/src/indexer.cpp
+++ search_engine/src/indexer.cpp
@@ -15,13 +15,12 @@ using std::array, std::logic_error, std::size_t, std::string_view;
 
 index make_index(const char * const texts_file) {
     static constexpr const char
-        *locale = "en_US.utf8",
         *invalid = "make_index: invalid JSON",
         *empty = "make_index: empty string";
     static array<char, 1UL << 21> buffer; // 2 MiB = 512 * 4 KiB
 
-    if (std::setlocale(LC_ALL, locale) == nullptr) [[unlikely]]
-        throw runtime_error("unable to set locale");
+    if (std::setlocale(LC_ALL, "en_US.utf8") == nullptr) [[unlikely]]
+        throw runtime_error("make_index: unable to set locale");
 
     const memmap map(texts_file);
     const char *first = map.data();

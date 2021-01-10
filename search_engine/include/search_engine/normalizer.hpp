fa9b2b4f74f7585572cacf7da96447576c2debad
Nikita <nikitasa1997@gmail.com>
2021-01-10 15:45:57 +0300

    next

--- search_engine/include/search_engine/normalizer.hpp
+++ search_engine/include/search_engine/normalizer.hpp
@@ -1,13 +1,9 @@
 #ifndef SEARCH_ENGINE_NORMALIZER_HPP
 #define SEARCH_ENGINE_NORMALIZER_HPP
 
-#include <cassert> // static_assert
-
-static_assert(__STDC_ISO_10646__ >= 201103L); // Unicode revision
-
+template<typename Invocable>
 class normalizer final {
 public:
-    static char *normalize(const char *, const char *, char *) noexcept;
 };
 
 #endif

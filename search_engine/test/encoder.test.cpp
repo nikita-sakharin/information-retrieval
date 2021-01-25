befd4816843af6f34b12f76fec7221fa70b2bb8a
Nikita <nikitasa1997@gmail.com>
2021-01-25 15:51:19 +0300

    next

--- search_engine/test/encoder.test.cpp
+++ search_engine/test/encoder.test.cpp
@@ -1,67 +1,76 @@
 #include <algorithm> // for_each
 #include <functional> // function
-#include <string> // string, wstring
-#include <string_view> // string_view, wstring_view
+#include <string> // basic_string
+#include <string_view> // basic_string_view
 #include <system_error> // system_error
 
 #include <gtest/gtest.h>
 
 #include <search_engine/encoder.hpp>
 
-using std::for_each, std::function, std::string, std::string_view,
-    std::system_error, std::wstring, std::wstring_view;
+using std::for_each, std::function, std::basic_string, std::basic_string_view,
+    std::system_error;
 
-static wstring encode_string(string_view);
-static string encode_wstring(wstring_view);
+template<typename From, typename To>
+static basic_string<To> convert(basic_string_view<From>);
+
+TEST(EncoderTest, Identity) {
+    ASSERT_EQ((convert<char, char>(
+        "The quick brown fox jumps over the lazy dog."
+        )), "The quick brown fox jumps over the lazy dog."
+    );
+    ASSERT_EQ((convert<char, char>(
+        "Съешь еще этих мягких французских булок, да выпей чаю."
+        )), "Съешь еще этих мягких французских булок, да выпей чаю."
+    );
+    ASSERT_EQ((convert<wchar_t, wchar_t>(
+        L"The quick brown fox jumps over the lazy dog."
+        )), L"The quick brown fox jumps over the lazy dog."
+    );
+    ASSERT_EQ((convert<wchar_t, wchar_t>(
+        L"Съешь еще этих мягких французских булок, да выпей чаю."
+        )), L"Съешь еще этих мягких французских булок, да выпей чаю."
+    );
+}
 
 TEST(EncoderTest, EncodeString) {
-    ASSERT_EQ(encode_string(
+    ASSERT_EQ((convert<char, wchar_t>(
         "The quick brown fox jumps over the lazy dog."
-        ), L"The quick brown fox jumps over the lazy dog."
+        )), L"The quick brown fox jumps over the lazy dog."
     );
-    ASSERT_EQ(encode_string(
+    ASSERT_EQ((convert<char, wchar_t>(
         "Съешь еще этих мягких французских булок, да выпей чаю."
-        ), L"Съешь еще этих мягких французских булок, да выпей чаю."
+        )), L"Съешь еще этих мягких французских булок, да выпей чаю."
     );
 }
 
 TEST(EncoderTest, EncodeWstring) {
-    ASSERT_EQ(encode_wstring(
+    ASSERT_EQ((convert<wchar_t, char>(
         L"The quick brown fox jumps over the lazy dog."
-        ), "The quick brown fox jumps over the lazy dog."
+        )), "The quick brown fox jumps over the lazy dog."
     );
-    ASSERT_EQ(encode_wstring(
+    ASSERT_EQ((convert<wchar_t, char>(
         L"Съешь еще этих мягких французских булок, да выпей чаю."
-        ), "Съешь еще этих мягких французских булок, да выпей чаю."
+        )), "Съешь еще этих мягких французских булок, да выпей чаю."
     );
 }
 
 TEST(EncoderTest, Throw) {
-    ASSERT_THROW(encode_string("\x80"), system_error);
-    ASSERT_THROW(encode_string("\xC0\x80"), system_error);
-    ASSERT_THROW(encode_string("\xE0\x80\x80"), system_error);
-    ASSERT_THROW(encode_string("\xF0\x80\x80\x80"), system_error);
+    ASSERT_THROW((convert<char, wchar_t>("\x80")), system_error);
+    ASSERT_THROW((convert<char, wchar_t>("\xC0\x80")), system_error);
+    ASSERT_THROW((convert<char, wchar_t>("\xE0\x80\x80")), system_error);
+    ASSERT_THROW((convert<char, wchar_t>("\xF0\x80\x80\x80")), system_error);
 }
 
-static wstring encode_string(const string_view str) {
-    wstring wbuffer;
-    encoder<char, wchar_t, function<void(wchar_t)>> string_encoder(
-        [&wbuffer](const wchar_t wc) constexpr -> void {
-            wbuffer.push_back(wc);
+template<typename From, typename To>
+static basic_string<To> convert(const basic_string_view<From> str) {
+    basic_string<To> buffer;
+    encoder<From, To, function<void(To)>> string_encoder(
+        [&buffer](const To c) constexpr -> void {
+            buffer.push_back(c);
         }
     );
     for_each(str.cbegin(), str.cend(), string_encoder);
 
-    return wbuffer;
-}
-
-static string encode_wstring(const wstring_view wstr) {
-    string buffer;
-    encoder<wchar_t, char, function<void(char)>> string_encoder(
-        [&buffer](const char c) constexpr -> void {
-            buffer.push_back(c);
-        }
-    );
-    for_each(wstr.cbegin(), wstr.cend(), string_encoder);
     return buffer;
 }

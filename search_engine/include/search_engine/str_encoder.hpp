9248a75bfbcd38639708b0b413d54b6ff4e182c8
Nikita <nikitasa1997@gmail.com>
2021-01-26 13:45:52 +0300

    next

--- search_engine/include/search_engine/str_encoder.hpp
+++ search_engine/include/search_engine/str_encoder.hpp
@@ -1,13 +1,14 @@
 #ifndef SEARCH_ENGINE_STR_ENCODER_HPP
 #define SEARCH_ENGINE_STR_ENCODER_HPP
 
-#include <cerrno> // errno
 #include <climits> // MB_LEN_MAX
 #include <clocale> // LC_ALL, setlocale
 #include <cstddef> // size_t
 #include <cwchar> // mbrtowc, mbstate_t, wcrtomb
 
 #include <algorithm> // for_each
+#include <string> // basic_string
+#include <string_view> // basic_string_view
 #include <system_error> // generic_category, system_error
 #include <type_traits> // is_invocable_r_v, is_nothrow_*_v, is_same_v
 
@@ -17,8 +18,7 @@ static_assert(__STDC_ISO_10646__ >= 201103L,
 template<typename From, typename To, typename Invocable>
 class str_encoder final {
 public:
-    constexpr str_encoder() noexcept(
-        std::is_nothrow_default_constructible_v<Invocable>) = default;
+    constexpr str_encoder() = default;
     constexpr str_encoder(const Invocable &invocable) noexcept(
         std::is_nothrow_copy_constructible_v<Invocable>);
     constexpr str_encoder(const str_encoder &) noexcept(
@@ -44,10 +44,13 @@ private:
         "template arguments From and To must both have type char or wchar_t"
     );
     static_assert(
-        std::is_invocable_r_v<void, Invocable, std::basic_string<To>>,
-        "invocable must have signature void(To)"
+        std::is_invocable_r_v<void, Invocable, std::basic_string<To> &>,
+        "invocable must have signature void(std::basic_string<To> &)"
     );
 
+    static constexpr std::size_t capacity = 1UL << 16;
+
+    std::basic_string<To> buffer(capacity, static_cast<To>('\0'));
     std::mbstate_t state{};
     Invocable invocable_{};
 };
@@ -58,32 +61,22 @@ constexpr str_encoder<From, To, Invocable>::str_encoder(
 ) noexcept(
     std::is_nothrow_copy_constructible_v<Invocable>
 ) : invocable_(invocable) {
-    if (std::setlocale(LC_ALL, "en_US.utf8") == nullptr) [[unlikely]]
-        throw std::logic_error("str_encoder::str_encoder: unable to set locale");
+    using std::logic_error, std::setlocale;
+
+    if (setlocale(LC_ALL, "en_US.utf8") == nullptr) [[unlikely]]
+        throw logic_error("str_encoder::str_encoder: unable to set locale");
 }
 
 template<typename From, typename To, typename Invocable>
 constexpr void str_encoder<From, To, Invocable>::operator()(
-    const From from
+    std::basic_string_view<From> from
 ) {
     using std::array, std::for_each, std::generic_category, std::is_same_v,
         std::mbrtowc, std::size_t, std::system_error, std::wcrtomb;
     constexpr const char *what = "str_encoder::operator()";
 
     if constexpr (is_same_v<From, char> && is_same_v<To, wchar_t>) {
-        wchar_t wc;
-        const size_t returns = mbrtowc(&wc, &from, 1U, &state);
-        if (returns == static_cast<size_t>(-1)) [[unlikely]]
-            throw system_error(errno, generic_category(), what);
-        if (returns == static_cast<size_t>(-2))
-            return;
-        invocable_(wc);
     } else if constexpr (is_same_v<From, wchar_t> && is_same_v<To, char>) {
-        array<char, MB_LEN_MAX> s;
-        const size_t returns = wcrtomb(s.data(), from, &state);
-        if (returns == static_cast<size_t>(-1)) [[unlikely]]
-            throw system_error(errno, generic_category(), what);
-        for_each(s.cbegin(), s.cbegin() + returns, invocable_);
     } else
         invocable_(from);
 }

dd8428c0b72993a107be52fab21051f8f9f1ae0b
Nikita <nikitasa1997@gmail.com>
2021-01-31 21:10:51 +0300

    next

--- search_engine/include/search_engine/str_encoder.hpp
+++ search_engine/include/search_engine/str_encoder.hpp
@@ -1,10 +1,10 @@
 #ifndef SEARCH_ENGINE_STR_ENCODER_HPP
 #define SEARCH_ENGINE_STR_ENCODER_HPP
 
-#include <clocale> // LC_ALL, setlocale
 #include <cstddef> // size_t
 #include <cwchar> // mbstowcs, wcstombs
 
+#include <bit> // bit_ceil
 #include <string> // basic_string
 #include <system_error> // generic_category, system_error
 #include <type_traits> // is_invocable_r_v, is_nothrow_*_v, is_same_v
@@ -45,10 +45,11 @@ private:
         "invocable must have signature void(std::basic_string<To> &)"
     );
 
-    static constexpr std::size_t capacity = 1U << 12;
+    template<typename Encoder>
+    constexpr void encode(const std::basic_string<From> &, Encoder);
 
     std::basic_string<To> buffer =
-        std::basic_string<To>(capacity, static_cast<To>('\0'));
+        std::basic_string<To>(1U << 12, static_cast<To>('\0'));
     Invocable invocable_{};
 };
 
@@ -57,48 +58,48 @@ constexpr str_encoder<From, To, Invocable>::str_encoder(
     const Invocable &invocable
 ) noexcept(
     std::is_nothrow_copy_constructible_v<Invocable>
-) : invocable_(invocable) {
-    using std::logic_error, std::setlocale;
-    constexpr const char *locale = "en_US.utf8";
-
-    if (setlocale(LC_ALL, locale) == nullptr) [[unlikely]]
-        throw logic_error("str_encoder::str_encoder: unable to set locale");
-}
+) : invocable_(invocable) {}
 
 template<typename From, typename To, typename Invocable>
 constexpr void str_encoder<From, To, Invocable>::operator()(
     const std::basic_string<From> &from
 ) {
-    using std::generic_category, std::is_same_v, std::mbstowcs, std::size_t,
-        std::system_error, std::wcstombs;
-    constexpr const char *what = "str_encoder::operator()";
+    using std::is_same_v, std::mbstowcs, std::wcstombs;
 
-    if constexpr (is_same_v<From, char> && is_same_v<To, wchar_t>) {
-        const size_t max_size = from.size();
-        buffer.resize(max_size);
-        const size_t size = mbstowcs(buffer.data(), from.c_str(), max_size);
-        if (size == static_cast<size_t>(-1)) [[unlikely]]
-            throw system_error(errno, generic_category(), what);
-        buffer.resize(size);
-        invocable_(buffer);
-    } else if constexpr (is_same_v<From, wchar_t> && is_same_v<To, char>) {
-        const size_t max_size = from.size() * MB_CUR_MAX;
-        buffer.resize(max_size);
-        const size_t size = wcstombs(buffer.data(), from.c_str(), max_size);
-        if (size == static_cast<size_t>(-1)) [[unlikely]]
-            throw system_error(errno, generic_category(), what);
-        buffer.resize(size);
-        invocable_(buffer);
-    } else {
-        buffer = from;
-        invocable_(buffer);
-    }
+    if constexpr (is_same_v<From, char> && is_same_v<To, wchar_t>)
+        encode(from, mbstowcs);
+    else if constexpr (is_same_v<From, wchar_t> && is_same_v<To, char>)
+        encode(from, wcstombs);
+    else
+        invocable_(buffer = from);
 }
 
 template<typename From, typename To, typename Invocable>
 constexpr Invocable str_encoder<From, To, Invocable>::get_invocable(
 ) const noexcept(std::is_nothrow_copy_assignable_v<Invocable>) {
     return invocable_;
+}
+
+template<typename From, typename To, typename Invocable>
+template<typename Encoder>
+constexpr void str_encoder<From, To, Invocable>::encode(
+    const std::basic_string<From> &from,
+    const Encoder encoder
+) {
+    using std::bit_ceil, std::generic_category, std::is_invocable_r_v,
+        std::is_same_v, std::size_t, std::system_error;
+    static_assert(!is_same_v<From, To>,
+        "template arguments From and To must have different types");
+    static_assert(is_invocable_r_v<size_t, Encoder, To *, const From *, size_t>,
+        "encoder must have signature size_t(To *, const From *, size_t)");
+
+    const size_t max_size = bit_ceil(from.size() * MB_CUR_MAX); // TODO
+    buffer.resize(max_size);
+    const size_t size = encoder(buffer.data(), from.c_str(), max_size);
+    if (size == static_cast<size_t>(-1)) [[unlikely]]
+        throw system_error(errno, generic_category(), "str_encoder::encode");
+    buffer.resize(size);
+    invocable_(buffer);
 }
 
 #endif

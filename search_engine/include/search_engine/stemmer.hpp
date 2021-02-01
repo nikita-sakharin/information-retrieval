9420f30b594c9cf44e57050b7651bff371410dd5
Nikita <nikitasa1997@gmail.com>
2021-02-01 22:27:03 +0300

    next

--- search_engine/include/search_engine/stemmer.hpp
+++ search_engine/include/search_engine/stemmer.hpp
@@ -1,13 +1,32 @@
 #ifndef SEARCH_ENGINE_STEMMER_HPP
 #define SEARCH_ENGINE_STEMMER_HPP
 
+#include <algorithm> // is_sorted
+
 class stemmer final {
 public:
     constexpr string &stem(string &) const noexcept;
+private:
+    std::array<endings, > = {
+        "ое",
+        "ые",
+        "ыми",
+        "ой",
+        "ый",
+        "ом",
+        "ым",
+        "ого",
+        "ому",
+        "ых",
+        "ою",
+        "ую",
+        "ая"
+    }
+    static_assert(std::is_sorted(endings.cbegin(), endings.cend()),
+        "endings must be sorted");
 };
 
 constexpr wstring &stemmer::stem(std::wstring &token) noexcept {
-    if (token.ends_with())
     return token;
 }
 

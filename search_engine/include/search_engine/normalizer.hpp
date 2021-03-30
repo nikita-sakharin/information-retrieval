b3e1fbf70c283779e0d89d8e4a68ba26caaa2933
Nikita <nikitasa1997@gmail.com>
2021-03-30 14:39:21 +0300

    next

--- search_engine/include/search_engine/normalizer.hpp
+++ search_engine/include/search_engine/normalizer.hpp
@@ -4,17 +4,32 @@
 #include <cwctype> // towlower
 
 #include <string> // wstring
+#include <type_traits> // is_invocable_r_v, is_nothrow_*_v
 
 template<typename Invocable>
 class normalizer final {
 public:
-    constexpr wstring &normalize(std::wstring &token) const noexcept;
+    constexpr void operator()(std::wstring &) noexcept(
+        std::is_nothrow_invocable_r_v<void, Invocable, std::wstring &>);
+
+private:
+    static_assert(__STDC_ISO_10646__ >= 201103L,
+        "Unicode version 2011 or later required"
+    );
+    static_assert(std::is_invocable_r_v<void, Invocable, std::wstring &>,
+        "Invocable must have signature void(wstring &)"
+    );
+
+    Invocable invocable_{};
 };
 
-constexpr wstring &normalizer::normalize(std::wstring &token) noexcept {
-    for (wchar_t &wc : token)
+template<typename Invocable>
+constexpr void normalizer::operator()(std::wstring &wcs) noexcept(
+    std::is_nothrow_invocable_r_v<void, Invocable, std::wstring &>
+) {
+    for (wchar_t &wc : wcs)
         wc = towlower(wc);
-    return token;
+    invocable_(buffer_);
 }
 
 #endif

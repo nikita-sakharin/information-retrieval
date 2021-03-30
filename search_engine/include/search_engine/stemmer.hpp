59299e083d7b95ca7a470dd7ee3f23483fd63d80
Nikita <nikitasa1997@gmail.com>
2021-03-30 16:59:51 +0300

    next

--- search_engine/include/search_engine/stemmer.hpp
+++ search_engine/include/search_engine/stemmer.hpp
@@ -2,33 +2,36 @@
 #define SEARCH_ENGINE_STEMMER_HPP
 
 #include <algorithm> // is_sorted
+#include <array> // array
+#include <string> // wstring
+#include <string_view> // wstring_view
 
 class stemmer final {
 public:
-    constexpr string &stem(string &) const noexcept;
+    constexpr std::wstring &stem(std::wstring &) const noexcept;
 
 private:
-    std::array<endings, > = {
-        "ое",
-        "ые",
-        "ыми",
-        "ой",
-        "ый",
-        "ом",
-        "ым",
-        "ого",
-        "ому",
-        "ых",
-        "ою",
-        "ую",
-        "ая"
-    }
-    static_assert(std::is_sorted(endings.cbegin(), endings.cend()),
-        "endings must be sorted");
+    std::array<, std::wstring_view> suffixes = { // " = {" or "{" ???
+        L"ое",
+        L"ые",
+        L"ыми",
+        L"ой",
+        L"ый",
+        L"ом",
+        L"ым",
+        L"ого",
+        L"ому",
+        L"ых",
+        L"ою",
+        L"ую",
+        L"ая"
+    };
+    static_assert(std::is_sorted(suffixes.cbegin(), suffixes.cend()),
+        "suffixes must be sorted");
 };
 
-constexpr wstring &stemmer::stem(std::wstring &token) noexcept {
-    return token;
+constexpr std::wstring &stemmer::stem(std::wstring &wcs) const noexcept {
+    return wcs;
 }
 
 #endif

d9a155747cd412a8bab2307f6d30bd2b5787c15a
Nikita <nikitasa1997@gmail.com>
2021-03-30 22:32:28 +0300

    next

--- search_engine/include/search_engine/normalizer.hpp
+++ search_engine/include/search_engine/normalizer.hpp
@@ -1,6 +1,7 @@
 #ifndef SEARCH_ENGINE_NORMALIZER_HPP
 #define SEARCH_ENGINE_NORMALIZER_HPP
 
+#include <cstddef> // size_t
 #include <cwctype> // towlower
 
 #include <string> // wstring
@@ -10,7 +11,22 @@
 template<typename Invocable>
 class normalizer final {
 public:
-    constexpr void operator()(std::wstring &) noexcept( // one-to-one or one-to-many ???
+    constexpr normalizer() noexcept(
+        std::is_nothrow_default_constructible_v<Invocable>) = default;
+    constexpr normalizer(const Invocable &) noexcept(
+        std::is_nothrow_copy_constructible_v<Invocable>);
+    constexpr normalizer(const normalizer &) noexcept(
+        std::is_nothrow_copy_constructible_v<Invocable>) = default;
+    constexpr normalizer(normalizer &&) noexcept(
+        std::is_nothrow_move_constructible_v<Invocable>) = default;
+    constexpr normalizer &operator=(const normalizer &) noexcept(
+        std::is_nothrow_copy_assignable_v<Invocable>) = default;
+    constexpr normalizer &operator=(normalizer &&) noexcept(
+        std::is_nothrow_move_assignable_v<Invocable>) = default;
+    constexpr ~normalizer() noexcept(
+        std::is_nothrow_destructible_v<Invocable>) = default;
+
+    constexpr void operator()(std::wstring &) noexcept( // TODO one-to-one or one-to-many ???
         std::is_nothrow_invocable_r_v<void, Invocable, std::size_t, std::wstring &>);
 
 private:
@@ -18,7 +34,7 @@ private:
         "Unicode version 2011 or later required"
     );
     static_assert(std::is_invocable_r_v<void, Invocable, std::size_t, std::wstring &>,
-        "Invocable must have signature void(wstring &)"
+        "Invocable must have signature void(size_t, wstring &)"
     );
 
     static constexpr std::wstring_view possessive_affix = L"'s";
@@ -28,14 +44,21 @@ private:
 };
 
 template<typename Invocable>
-constexpr void normalizer::operator()(std::wstring &wcs) noexcept(
+constexpr normalizer<Invocable>::normalizer(
+    const Invocable &invocable
+) noexcept(
+    std::is_nothrow_copy_constructible_v<Invocable>
+) : invocable_(invocable) {}
+
+template<typename Invocable>
+constexpr void normalizer<Invocable>::operator()(std::wstring &wcs) noexcept(
     std::is_nothrow_invocable_r_v<void, Invocable, std::size_t, std::wstring &>
 ) {
     using std::towlower;
 
     for (wchar_t &wc : wcs)
         wc = towlower(wc);
-    invocable_(position_++, buffer_);
+    invocable_(position_++, wcs);
 }
 
 #endif

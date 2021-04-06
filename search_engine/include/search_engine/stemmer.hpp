ff90e98dc45c3cc371d0ece832a7eb79a02a783a
Nikita <nikitasa1997@gmail.com>
2021-04-06 08:37:16 +0300

    next

--- search_engine/include/search_engine/stemmer.hpp
+++ search_engine/include/search_engine/stemmer.hpp
@@ -5,14 +5,39 @@
 #include <array> // array
 #include <string> // wstring
 #include <string_view> // wstring_view
+#include <type_traits> // is_invocable_r_v, is_nothrow_*_v
 
+template<typename Invocable>
 class stemmer final {
 public:
-    constexpr std::wstring &stem(std::wstring &) const noexcept;
+    constexpr stemmer() noexcept(
+        std::is_nothrow_default_constructible_v<Invocable>) = default;
+    constexpr explicit stemmer(const Invocable &) noexcept(
+        std::is_nothrow_copy_constructible_v<Invocable>);
+    constexpr stemmer(const stemmer &) noexcept(
+        std::is_nothrow_copy_constructible_v<Invocable>) = default;
+    constexpr stemmer(stemmer &&) noexcept(
+        std::is_nothrow_move_constructible_v<Invocable>) = default;
+    constexpr stemmer &operator=(const stemmer &) noexcept(
+        std::is_nothrow_copy_assignable_v<Invocable>) = default;
+    constexpr stemmer &operator=(stemmer &&) noexcept(
+        std::is_nothrow_move_assignable_v<Invocable>) = default;
+    constexpr ~stemmer() noexcept(
+        std::is_nothrow_destructible_v<Invocable>) = default;
+
+    constexpr void operator()(std::wstring &) noexcept(
+        std::is_nothrow_invocable_r_v<void, Invocable, std::wstring &>);
+
+    constexpr const Invocable &invocable() const noexcept;
+    constexpr Invocable &invocable() noexcept;
 
 private:
     static_assert(__STDC_ISO_10646__ >= 201103L,
         "Unicode version 2011 or later required"
+    );
+    static_assert(
+        std::is_invocable_r_v<void, Invocable, std::wstring &>,
+        "Invocable must have signature void(wstring &)"
     );
 
     static constexpr std::array<std::wstring_view, 18> suffixes {
@@ -20,6 +45,7 @@ private:
         L"es",
         L"ness",
         L"ly", // ???
+
         L"ое",
         L"ые",
         L"ыми",
@@ -50,10 +76,30 @@ private:
     static_assert(!suffixes.empty() && !suffixes.front().empty(),
         "all suffix must not be empty"
     );
+
+    Invocable invocable_{};
 };
 
-constexpr std::wstring &stemmer::stem(std::wstring &wcs) const noexcept {
-    return wcs;
+template<typename Invocable>
+constexpr stemmer<Invocable>::stemmer(const Invocable &invocable) noexcept(
+    std::is_nothrow_copy_constructible_v<Invocable>
+) : invocable_(invocable) {}
+
+template<typename Invocable>
+constexpr void stemmer<Invocable>::operator()(std::wstring &wcs) noexcept(
+    std::is_nothrow_invocable_r_v<void, Invocable, std::wstring &>
+) {
+    invocable_(wcs);
+}
+
+template<typename Invocable>
+constexpr const Invocable &stemmer<Invocable>::invocable() const noexcept {
+    return invocable_;
+}
+
+template<typename Invocable>
+constexpr Invocable &stemmer<Invocable>::invocable() noexcept {
+    return invocable_;
 }
 
 #endif

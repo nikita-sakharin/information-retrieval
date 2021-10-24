1b6e9dd68029dbc19802ebb55e711a0178562f51
Nikita <nikitasa1997@gmail.com>
2021-10-25 01:19:40 +0300

    next

--- search_engine/include/search_engine/stemmer.hpp
+++ search_engine/include/search_engine/stemmer.hpp
@@ -45,14 +45,14 @@ private:
 
     static constexpr std::size_t size_before_suffix = 2U;
 
-    static constexpr std::array<std::wstring_view, 54U> suffixes {
+    static constexpr std::array<std::wstring_view, 55U> suffixes {
         L"ing", L"al", L"s", L"es", L"ness", L"ly",
 
-        L"а", L"ила", L"е", L"ее", L"ие", L"ое", L"ые", L"и", L"ли", L"или",
-        L"ами", L"ими", L"ыми", L"ями", L"й", L"ей", L"ий", L"ой", L"ый", L"л",
-        L"ил", L"ам", L"ем", L"им", L"ом", L"ым", L"ям", L"ём", L"о", L"ого",
-        L"ло", L"ило", L"у", L"ому", L"ах", L"их", L"ых", L"ях", L"ы", L"ь",
-        L"ившись", L"ывшись", L"ю", L"ою", L"ую", L"ью", L"я", L"ая"
+        L"а", L"ила", L"ёв", L"е", L"ее", L"ие", L"ое", L"ые", L"и", L"ли",
+        L"или", L"ами", L"ими", L"ыми", L"ями", L"й", L"ей", L"ий", L"ой",
+        L"ый", L"л", L"ил", L"ам", L"ем", L"им", L"ом", L"ым", L"ям", L"ём",
+        L"о", L"ого", L"ло", L"ило", L"у", L"ому", L"ах", L"их", L"ых", L"ях",
+        L"ы", L"ь", L"ившись", L"ывшись", L"ю", L"ою", L"ую", L"ью", L"я", L"ая"
     };
     static_assert(
         std::is_sorted(suffixes.cbegin(), suffixes.cend(),
